8e039c3993619905aa5b5a9bf10ecff4da4de238
CaoYanfei <onefours@gmail.com>
2018-02-27 19:30:30 +0800

    fix the max invoke timeout to 60 seconds

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var spider_yinjianhui = require('./spider_yinjianhui');
 var spider_zhenjianhui = require('./spider_zhenjianhui');
 var notify_google_calendar_by_ifttt = require('./notify_google_calendar_by_ifttt');
 exports.handler = function(event, context, callback) {
-  if(event){
+  if(event&&event.resources&&event.resources[0]){
     console.log("触发器是"+event.resources[0]);
     if(event.resources[0].match("notify_google_calender")){//使用了土办法，当event是google calender时，触发特定的脚本
       notify_google_calendar_by_ifttt();
